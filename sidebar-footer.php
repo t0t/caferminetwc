ce8e276baf7e3e8e95189699d0d8a3b2d72a0238
Sergio Forés <sergiofores@gmail.com>
2015-10-05 20:25:48 +0200

    Fixed flex layout bugs :cloud:

--- sidebar-footer.php
+++ sidebar-footer.php
@@ -1,17 +1,13 @@
 <div class="sidebar sidebar-horizontal">
-
-    <?php dynamic_sidebar('Footer Sidebar'); ?>
-
-    <div class="widget social">
-      <h4 class="widget-title">Contactanos</h4>
-  		<a href="https://www.facebook.com/CaFerminet" target="_blank">
-  				<svg class="icon">
-  						<use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/icons/icons.svg#icon-facebook"></use>
-  				</svg></a>
-  		<a href="mailto:sergiofores@gmail.com">
-  			<svg class="icon">
-  				<use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/icons/icons.svg#icon-envelope"></use>
-  		</svg></a>
-
-  	</div>
+  <?php dynamic_sidebar('Footer Sidebar'); ?>
+  <div class="widget widget--social">
+		<a href="https://www.facebook.com/CaFerminet" target="_blank">
+			<svg class="icon">
+				<use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/icons/icons.svg#icon-facebook"></use>
+			</svg> Facebook</a>
+		<a href="mailto:sergiofores@gmail.com">
+			<svg class="icon">
+			 <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/icons/icons.svg#icon-envelope"></use>
+		</svg> Contacto</a>
+	</div>
 </div>
