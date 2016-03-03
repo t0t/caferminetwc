c103085dba654f9a6f7d2e43f4f76af5f35b4fae
Sergio Forés Raga <sergiofores@gmail.com>
2016-03-03 13:07:41 +0100

    Layout en pagina

--- functions.php
+++ functions.php
@@ -57,6 +57,9 @@ remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
 remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
+// remove_filter( 'the_content', 'wpautop' );
+// remove_filter( 'the_excerpt', 'wpautop' );
+
 // Register Sidebar
 register_sidebar(array(
     'name' => 'Main Sidebar',
