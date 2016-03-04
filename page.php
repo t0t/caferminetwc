6a5312b6925c6a7f24cbb61ce1a2d3ca51568e96
Sergio Forés Raga <sergiofores@gmail.com>
2016-03-04 13:36:23 +0100

    Layout

--- page.php
+++ page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <main class="site-main" role="main">
   <?php if ( have_posts() ) : while( have_posts() ) : the_post(); ?>
-    <?php get_template_part( 'content', get_post_format() ); ?>
+    <?php get_template_part( 'content', 'page' ); ?>
   <?php endwhile; ?>
   <?php else : ?>
     <?php get_template_part( 'content', 'none' ); ?>
